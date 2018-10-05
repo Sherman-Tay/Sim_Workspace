e49058f6a51fe5600c6f38618594e54d51a13063
sherman-tay <shermantayjy@gmail.com>
2018-10-05 16:24:11 +0800

    initial commit for workspace

---
+++ build/prius_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sherman/simulation_ws/src/prius_description
+# Build directory: /home/sherman/simulation_ws/build/prius_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/track01/CMakeFiles/track01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/track01.dir/src/world_plugin.cpp.o"
+  "/home/sherman/simulation_ws/devel/lib/libtrack01.pdb"
+  "/home/sherman/simulation_ws/devel/lib/libtrack01.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/track01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
